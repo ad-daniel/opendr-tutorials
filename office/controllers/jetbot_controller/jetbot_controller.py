4cf2405345554b437397e382537e6bd2a11b14ca
ad-daniel <daniel.dias@epfl.ch>
2023-02-07 17:38:31 +0100

    cleaup

--- office/controllers/jetbot_controller/jetbot_controller.py
+++ office/controllers/jetbot_controller/jetbot_controller.py
@@ -19,8 +19,8 @@ import numpy as np
 import os
 import base64
 
-
-models = {
+DISPLAY_IMAGE_PATH = os.getcwd() + '/display.jpg'
+MODELS = {
     'Keyboard': 'translation -0.7 0.5 0.52 rotation 0 1 0 -0.12',
     'Laptop': 'translation -0.58 0.5 0.5 rotation 0 0 1 -2.16 controller "<none>"',
     'BeerBottle': 'translation -0.58 0.5 0.5 rotation 0 1 0 0',
@@ -32,61 +32,22 @@ models = {
 }
 
 
-def spawn_object(name):
-    # remove existing node
-    target = robot.getFromDef('TARGET')
-    if target:
-        target.remove()
-    # spawn new object
-    root_children_field.importMFNodeFromString(-1, f'DEF TARGET {name} {{ {models[name]} }}')
-
-
 def send_image_to_display(robot, display):
-    """Send display image to the robot window"""
-    display.imageSave(None, display_image_path)
-
-    with open(display_image_path, 'rb') as f:
+    display.imageSave(None, DISPLAY_IMAGE_PATH)
+    with open(DISPLAY_IMAGE_PATH, 'rb') as f:
         fileString64 = base64.b64encode(f.read()).decode()
         robot.wwiSendText('image[display]]:data:image/jpeg;base64,' + fileString64)
 
 
-display_image_path = os.getcwd() + '/display.jpg'
-
-robot = Supervisor()
-timestep = int(robot.getBasicTimeStep())
-
-learner = NanodetLearner(model_to_use='m', device='cpu')
-learner.load("./nanodet_m", verbose=True)
-
-camera = Camera("camera")
-camera.enable(2 * timestep)
-width, height = camera.getWidth(), camera.getHeight()
-print(width, height, learner.classes)
-
-# Get the display device.
-display = robot.getDevice('display')
-display.setFont('Lucida Console', 15, True)
-
-root_node = robot.getRoot()
-root_children_field = root_node.getField('children')
-
-camera_node = robot.getFromDef("CAMERA")
-noise_field = camera_node.getField('noise')
-lens_field = camera_node.getField('lens').getSFNode().getField('radialCoefficients')
-current_detection = None
-
-light_node = robot.getFromDef("LIGHT")
-light_translation_field = light_node.getField('translation')
-light_color_field = light_node.getField('pointLightColor')
-
-while robot.step(timestep) != -1:
-    image = camera.getImage()
-
-    # retrieve messages from robot window
+def handle_wwi_messages():
     message = robot.wwiReceiveText()
     while message:
         if message.startswith('spawn:'):
-            spawn_object(message[6:])
+            name = message[6:]
+            target = robot.getFromDef('TARGET')
+            if target:  # remove existing node
+                target.remove()
+            root_children_field.importMFNodeFromString(-1, f'DEF TARGET {name} {{ {MODELS[name]} }}')  # spawn new object
         elif message.startswith('noise:'):
             noise_field.setSFFloat(float(message[6:]))
         elif message.startswith('radial-coefficient:'):
@@ -100,44 +61,73 @@ while robot.step(timestep) != -1:
             light_color_field.setSFColor(value)
         message = robot.wwiReceiveText()
 
-    if image:
-        frame = np.frombuffer(image, np.uint8).reshape((height, width, 4))
-        frame = frame[:, :, :3]
-        (h, w) = frame.shape[:2]
 
-        if current_detection:
-            # erase the previous drawing by setting the pixels alpha value to 0 (transparent).
-            display.setAlpha(0.0)
-            display.drawRectangle(current_detection[0], current_detection[1], current_detection[2], current_detection[3])
-            current_detection = None
+robot = Supervisor()
+timestep = int(robot.getBasicTimeStep())
 
-        img = Image(frame)
-        boxes = learner.infer(input=img)
+# enable devices
+camera = Camera('camera')
+camera.enable(2 * timestep)
+width, height = camera.getWidth(), camera.getHeight()
 
-        ir = display.imageNew(image, Display.BGRA, width, height)
-        display.imagePaste(ir, 0, 0, False)
+display = robot.getDevice('display')
+display.setFont('Lucida Console', 16, True)
 
-        if len(boxes) > 0:
-            bb = boxes[0].coco()
-            current_detection = bb['bbox']
+# get node/field references
+root_node = robot.getRoot()
+root_children_field = root_node.getField('children')
 
-            for i in range(len(boxes)):
-                if learner.classes[bb['category_id']] == 'dining_table' or float(boxes[i].confidence) < 0.5:
-                    continue
+camera_node = robot.getFromDef('CAMERA')
+noise_field = camera_node.getField('noise')
+lens_field = camera_node.getField('lens').getSFNode().getField('radialCoefficients')
+current_detection = None
 
-                # show detection in the display
-                display.setAlpha(1.0)
-                display.setColor(0x00FFFF)
-                display.drawRectangle(current_detection[0], current_detection[1], current_detection[2], current_detection[3])
-                display.drawText(learner.classes[bb['category_id']], current_detection[0], current_detection[1] - 20)
-                display.setColor(0xFF0000)
+light_node = robot.getFromDef("LIGHT")
+light_translation_field = light_node.getField('translation')
+light_color_field = light_node.getField('pointLightColor')
 
-                #print('bounding box:', bb['bbox'])
-                print('class:', learner.classes[bb['category_id']], 'confidence:', boxes[0].confidence)
+robot.step(timestep)
 
-        send_image_to_display(robot, display)
-        display.imageDelete(ir)
+# prepare Nanodet learner
+learner = NanodetLearner(model_to_use='m', device='cpu')
+learner.load("./nanodet_m", verbose=True)
+
+while robot.step(timestep) != -1:
+    handle_wwi_messages()  # handle messages from robot window
+
+    image = camera.getImage()
+    frame = np.frombuffer(image, np.uint8).reshape((height, width, 4))
+
+    if current_detection:
+        # erase the previous drawing by setting the pixels alpha value to 0 (transparent).
+        display.setAlpha(0.0)
+        display.drawRectangle(current_detection[0], current_detection[1], current_detection[2], current_detection[3])
+        current_detection = None
+
+    # detect objects
+    result = learner.infer(input=Image(frame))
+
+    # copy raw image into display
+    ir = display.imageNew(image, Display.BGRA, width, height)
+    display.imagePaste(ir, 0, 0, False)
+
+    for box in result:
+        bounding_box = box.coco()
+        if learner.classes[bounding_box['category_id']] == 'dining_table' or float(box.confidence) < 0.5:
+            continue
+        # draw detection box in the display image
+        current_detection = bounding_box['bbox']
+        display.setAlpha(1.0)
+        display.setColor(0x00FFFF)
+        display.drawRectangle(current_detection[0], current_detection[1], current_detection[2], current_detection[3])
+        display.drawText(learner.classes[bounding_box['category_id']], current_detection[0], current_detection[1] - 20)
+        display.setColor(0xFF0000)
+
+        # print('class:', learner.classes[bb['category_id']], 'confidence:', boxes[0].confidence)
+
+    send_image_to_display(robot, display)
+    display.imageDelete(ir)
 
 # cleanup
-if (os.path.exists(display_image_path)):
-    os.remove(display_image_path)
+if (os.path.exists(DISPLAY_IMAGE_PATH)):
+    os.remove(DISPLAY_IMAGE_PATH)
