b1ad204931f73074277b1469aa91b02102291daa
ad-daniel <daniel.dias@epfl.ch>
2023-02-08 10:25:06 +0100

    General fixes

--- office/controllers/jetbot_controller/jetbot_controller.py
+++ office/controllers/jetbot_controller/jetbot_controller.py
@@ -19,16 +19,16 @@ import numpy as np
 import os
 import base64
 
+TIMESTEP = 64
 DISPLAY_IMAGE_PATH = os.getcwd() + '/display.jpg'
-MODELS = {
-    'Keyboard': 'translation -0.7 0.5 0.52 rotation 0 1 0 -0.12',
-    'Laptop': 'translation -0.58 0.5 0.5 rotation 0 0 1 -2.16 controller "<none>"',
-    'BeerBottle': 'translation -0.58 0.5 0.5 rotation 0 1 0 0',
-    'Cat': 'translation -0.62 0.46 0.56 rotation 0 0 1 -2.88 scale 0.75',
-    'SoccerBall': 'translation 0 0 0 rotation 0 1 0 0',
-    'FlowerPot': 'translation -0.71 0.49 0.53 rotation 0 1 0 0',
-    'Clock': 'translation -0.54 0.46 0.67 rotation 0 0 1 2.618',
-    'Book': 'translation -0.51 0.52 0.51 rotation -0.12 -1 -0.12 1.58',
+LOCATIONS = {
+    'Keyboard': [-0.7, 0.5, 0.52],
+    'Laptop': [-0.58, 0.5, 0.5],
+    'BeerBottle': [-0.42, 0.49, 0.5],
+    'Cat': [-0.57, 0.40, 0.56],
+    'FlowerPot': [-0.71, 0.49, 0.53],
+    'Clock': [-0.54, 0.46, 0.67],
+    'TennisRacket': [-0.53, 0.42, 0.62]
 }
 
 
@@ -40,14 +40,15 @@ def send_image_to_display(robot, display):
 
 
 def handle_wwi_messages():
+    global current_object
     message = robot.wwiReceiveText()
     while message:
         if message.startswith('spawn:'):
             name = message[6:]
-            target = robot.getFromDef('TARGET')
-            if target:  # remove existing node
-                target.remove()
-            root_children_field.importMFNodeFromString(-1, f'DEF TARGET {name} {{ {MODELS[name]} }}')  # spawn new object
+            if current_object:  # hide current object
+                robot.getFromDef(current_object).getField('translation').setSFVec3f([0, 0, -2])
+            current_object = name.upper()
+            robot.getFromDef(current_object).getField('translation').setSFVec3f(LOCATIONS[name])
         elif message.startswith('noise:'):
             noise_field.setSFFloat(float(message[6:]))
         elif message.startswith('radial-coefficient:'):
@@ -63,20 +64,16 @@ def handle_wwi_messages():
 
 
 robot = Supervisor()
-timestep = int(robot.getBasicTimeStep())
 
 # enable devices
 camera = Camera('camera')
-camera.enable(2 * timestep)
+camera.enable(2 * TIMESTEP)
 width, height = camera.getWidth(), camera.getHeight()
 
 display = robot.getDevice('display')
 display.setFont('Lucida Console', 16, True)
 
 # get node/field references
-root_node = robot.getRoot()
-root_children_field = root_node.getField('children')
-
 camera_node = robot.getFromDef('CAMERA')
 noise_field = camera_node.getField('noise')
 lens_field = camera_node.getField('lens').getSFNode().getField('radialCoefficients')
@@ -86,13 +83,15 @@ light_node = robot.getFromDef("LIGHT")
 light_translation_field = light_node.getField('translation')
 light_color_field = light_node.getField('pointLightColor')
 
-robot.step(timestep)
+robot.step(TIMESTEP)
 
 # prepare Nanodet learner
 learner = NanodetLearner(model_to_use='m', device='cpu')
 learner.load("./nanodet_m", verbose=True)
+print(learner.classes)
+current_object = None
 
-while robot.step(timestep) != -1:
+while robot.step(TIMESTEP) != -1:
     handle_wwi_messages()  # handle messages from robot window
 
     image = camera.getImage()
@@ -123,7 +122,7 @@ while robot.step(timestep) != -1:
         display.drawText(learner.classes[bounding_box['category_id']], current_detection[0], current_detection[1] - 20)
         display.setColor(0xFF0000)
 
-        # print('class:', learner.classes[bb['category_id']], 'confidence:', boxes[0].confidence)
+        # print('class:', learner.classes[bounding_box['category_id']], 'confidence:', box.confidence)
 
     send_image_to_display(robot, display)
     display.imageDelete(ir)
